9357f8dc181ba133085244efb5bc2ae7b766589d
Semira Musema <Semiramusemaa@gmail.com>
2025-09-13 08:11:42 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function login()  {
+console.log("welcome to console!");
+let name= prompt("enter your name:");
+let age= prompt("enter your age:");
+alert("welcome " + name +"!");
+}
+function varvsconst()
+{
+  var city= "Addis Ababa";
+  console.log("Orginal var city:", city);
+  city = "Dire dawa";
+  console.log("update var city:", city);
+
+  const country = "Ethiopia";
+  console.log("const country:",country);
+
+  //country= "kenya";
+}
